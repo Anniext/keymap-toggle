f26cc329fcd704d35324423a2eda4639a58826ca
xt <1014797089@qq.com>
2023-10-19 05:07:44 +0800

    feat( MacOS ): 增加cpp文件注释自动切换输入法

---
+++ lua/keymap-toggle.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+function M.switch()
+  vim.fn.system('osascript -e \'tell application "System Events" to keystroke " " using {control down, shift down}\'')
+end
+
+function M.setup()
+  vim.cmd('command! AutoToggle :lua require(\'plugins.auto_toggle\').switch()')
+end
+vim.cmd('autocmd FileType cpp inoremap // <Esc>:AutoToggle<CR>i//')
+return M
